bc846e7b15c25c68f36943f19fac96272bd6bfea
Wilson Coutinho <wilsoncoutinh@gmail.com>
2026-02-04 20:48:15 -0300

    Teste

--- Laywill/src/lib/supabase.ts
+++ Laywill/src/lib/supabase.ts
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // SUBSTITUA PELOS SEUS DADOS DO DASHBOARD DO SUPABASE
 const SUPABASE_URL = "https://duiaoahiuaurreglqclk.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_JUvMgUgTxII0mn8GnSJjfA_hKeqYmvK";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR1aWFvYWhpdWF1cnJlZ2xxY2xrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMDE1NDEsImV4cCI6MjA4NTc3NzU0MX0.xCnzJ0wAMd-vrlnw3p75kHCrSLEa2Pi035drAlNnN9A";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: {
